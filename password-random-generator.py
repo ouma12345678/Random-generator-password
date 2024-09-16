1a0c5ad40f23bbc92eb45641839198910847373a
Oumayma Mimouni <134392105+ouma12345678@users.noreply.github.com>
2024-09-16 12:54:08 +0100

    Update password-random-generator.py

--- password-random-generator.py
+++ password-random-generator.py
@@ -1,7 +1,7 @@
 import random
 import string
 
-def generate_password(length=12):
+def generate_password(length=10):
     characters = string.ascii_letters + string.digits + string.punctuation
     password = ''.join(random.choice(characters) for i in range(length))
     return password
